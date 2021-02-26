f9822c1cb442f40036f6acb4556061235f1706a1
hdahle <hakon.dahle@gmail.com>
2021-02-26 13:15:20 +0000

    changed text prompt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello there, Wooooooooooorld!');
+  response.end('Hello, trying to deploy from GitHub');
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
